a4838e90c465c6faf41154255f0edba9b69607d3
Yanglong <liyanglong@ris-Mac-mini.local>
2020-06-08 00:31:59 +1200

    update projects

--- js/vueScript.js
+++ js/vueScript.js
@@ -2,8 +2,14 @@ var app = new Vue({
     el: '#projects',
     data: {
         projs: [
-        { name: 'Mario', code_link:'https://codepen.io/ohmama/full/eYNxqvm',img_href:'./assets/mario.png'},
-        { name: 'Bizarre Calculator', code_link:'https://codepen.io/ohmama/full/abdOYxV',img_href:'./assets/calc.png'},
+        { name: 'Mario', code_link:'eYNxqvm',img_href:'./assets/mario.png'},
+        { name: 'Bizarre Calculator', code_link:'abdOYxV',img_href:'./assets/calc.png'},
+        { name: 'Clock', code_link:'oNbXyeb',img_href:'./assets/clock.png'},
+        { name: 'Books Finder', code_link:'YzwXvaN',img_href:'./assets/book.png'},
+        { name: 'Free Canvas', code_link:'dyGoKqO',img_href:'./assets/drawer.png'},
+        { name: 'Pieces Pic', code_link:'KKVpeEE',img_href:'./assets/piece.png'},
+        { name: 'Higher Striker', code_link:'vYLOrwJ',img_href:'./assets/striker.png'},
+        { name: 'Markdown Note', code_link:'GRoJGbQ',img_href:'./assets/markdown.png'},
         ]
     }
 })
