ff8406bdf08352b91aabd08761f9881076593c95
Yanglong <liyanglong@ris-Mac-mini.local>
2020-06-07 19:43:41 +1200

    init

---
+++ js/vueScript.js
@@ -0,0 +1,9 @@
+var app = new Vue({
+    el: '#projects',
+    data: {
+        projs: [
+        { name: 'Mario', code_link:'https://codepen.io/ohmama/full/eYNxqvm',img_href:'./assets/mario.png'},
+        { name: 'Bizarre Calculator', code_link:'https://codepen.io/ohmama/full/abdOYxV',img_href:'./assets/calc.png'},
+        ]
+    }
+})
