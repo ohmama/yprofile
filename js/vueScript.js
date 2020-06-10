6a44f82fa894201a8ed3a957830ddcd7c9f00434
LYL <MyMac>
2020-06-10 21:26:18 +1200

    update

--- js/vueScript.js
+++ js/vueScript.js
@@ -3,13 +3,15 @@ var app = new Vue({
     data: {
         projs: [
         { name: 'Mario', code_link:'eYNxqvm',img_href:'./assets/mario.png'},
-        { name: 'Bizarre Calculator', code_link:'abdOYxV',img_href:'./assets/calc.png'},
-        { name: 'Clock', code_link:'oNbXyeb',img_href:'./assets/clock.png'},
-        { name: 'Books Finder', code_link:'YzwXvaN',img_href:'./assets/book.png'},
-        { name: 'Free Canvas', code_link:'dyGoKqO',img_href:'./assets/drawer.png'},
-        { name: 'Pieces Pic', code_link:'KKVpeEE',img_href:'./assets/piece.png'},
         { name: 'Higher Striker', code_link:'vYLOrwJ',img_href:'./assets/striker.png'},
+        { name: 'Stay Home', git_link:'https://ohmama.github.io/stayhome/',img_href:'./assets/covid.jpg',icon:'fa-github'},
+        { name: 'Books Finder', code_link:'YzwXvaN',img_href:'./assets/book.jpg'},
+        { name: 'Free Canvas', code_link:'dyGoKqO',img_href:'./assets/drawer.png'},
+        { name: 'Pieces Pic', code_link:'KKVpeEE',img_href:'./assets/piece.jpg'},
         { name: 'Markdown Note', code_link:'GRoJGbQ',img_href:'./assets/markdown.png'},
+        { name: 'Clock', code_link:'oNbXyeb',img_href:'./assets/clock.png'},
+        { name: 'Bizarre Calculator', code_link:'abdOYxV',img_href:'./assets/calc.png'},
+
         ]
     }
 })
